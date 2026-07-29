9ef0007abb878cae618149900fdaf26b19d7045c
ashishkhadye33 <ashishkhadye33@gmail.com>
2026-07-29 12:21:12 +0530

    dotnetCore session 3

---
+++ dotnetCore.Training/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using dotnetCore.Training.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace dotnetCore.Training.Data
+{
+    //Database
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+
+    }
+}
