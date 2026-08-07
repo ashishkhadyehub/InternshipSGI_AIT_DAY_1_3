29f0e29dfc0787731c1b2599dcb1917b4c4eace9
ashishkhadye33 <ashishkhadye33@gmail.com>
2026-08-07 13:18:03 +0530

    dotnetCore session 7

---
+++ LA.UI/Controllers/ConceptsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LA.UI.Controllers
+{
+    public class ConceptsController : Controller
+    {
+        public IActionResult Index()
+        {
+            //ViewBag.Message = "this text is stored in ViewBag";
+            //ViewData["Message"] = "this text is stored in ViewData";
+            TempData["Message"] = "this text is stored in TempData";
+            return View();
+        }
+        public IActionResult Next()
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ LA.UI/Controllers/StateController.cs
@@ -0,0 +1,67 @@
+﻿using LA.Entities;
+using LA.Repositories.Implementations;
+using LA.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace LA.UI.Controllers
+{
+    public class StateController : Controller
+    {
+        private readonly ICountryRepo _countryRepo;
+        private readonly IStateRepo _stateRepo;
+
+        public StateController(ICountryRepo countryRepo, IStateRepo stateRepo)
+        {
+            _countryRepo = countryRepo;
+            _stateRepo = stateRepo;
+        }
+
+        public IActionResult Index()
+        {
+            var stateList = _stateRepo.GetAll();
+            return View(stateList);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var countryList = _countryRepo.GetAll();
+            ViewBag.CountryList = new SelectList(countryList, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(State state)
+        {
+            _stateRepo.Save(state);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var state = _stateRepo.GetById(id);
+            var countryList = _countryRepo.GetAll();
+            ViewBag.CountryList = new SelectList(countryList, "Id", "Name");
+            return View(state);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(State state)
+        {
+            _stateRepo.Edit(state);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var state = _stateRepo.GetById(id);
+            _stateRepo.RemoveData(state);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
